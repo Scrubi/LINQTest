1bccc6b8d0e21efd7f1d891dafb68dbd7f7fc2b2
Jani Soronen <soronenjani@gmail.com>
2020-04-14 14:44:29 +0300

    Add project files.

---
+++ LINQTest/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using TestDataGeneratorLibrary;
+using System.Linq;
+
+namespace LINQTest
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            List<Person> people = GenerateListOfPeople();
+            
+            var ages = people.Where(x => x.FirstName.StartsWith("S")&& x.Age.ToString().Contains(5.ToString())).ToList();
+            
+            //Console.WriteLine("Random Persons:\n");
+            //PrintPerson();
+
+            //Console.WriteLine("\nList of randomly generated people:\n");
+            //PrintPeople();
+
+            //Console.WriteLine("\nList of 200 People:\n");
+            //GenerateListOfPeople();
+            
+            foreach (var person in ages) 
+            {
+                Console.WriteLine($"{person.FirstName} {person.LastName}, {person.Age}, {person.gender}");
+            }
+            Console.ReadKey();
+        }
+
+        public static void PrintPerson() 
+        {
+            for (int i = 0; i < 10; i++) 
+            {
+                Console.WriteLine(TestDataGenerator.GenerateRandomPerson());
+            }
+        }
+
+        public static void PrintPeople() 
+        {
+            List<Person> personList = new List<Person>();
+           
+            for (int i = 0; i < 25; i++)
+            {
+                
+                Person persons = TestDataGenerator.GenerateRandomPerson();
+                personList.Add(persons);
+                Console.WriteLine(persons);
+               
+            }
+            
+            Console.WriteLine($"\nThis List had {personList.Count} people.");
+        }
+
+        public static List<Person> GenerateListOfPeople() 
+        {
+            int PeopleAmount = 200;
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < PeopleAmount; i++)
+            {
+                Person peoples = TestDataGenerator.GenerateRandomPerson();
+                people.Add(peoples);
+                
+            }
+            return people;
+        }
+    }
+}
